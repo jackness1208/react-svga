e963a866eaa512a0b353c47fc56cfa2977dfc903
liudaojie <liudaojie@yy.com>
2021-04-28 16:17:48 +0800

    fix: 修复 在同一页面内使用多个 react-svga 时 只显示最后一个的 bug

--- test/case/base/src/entry/index/index.jsx
+++ test/case/base/src/entry/index/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Svga } from '../../../../../../'
-import svgaIcon from './images/bubble.svga'
+import winIcon from './images/win.svga'
+import loseIcon from './images/lose.svga'
 
 const onHandle = (type) => {
   console.log(`on(${type})`)
 }
-const App = <Svga src={svgaIcon} option={{ loop: true }} on={onHandle} />
+const App = (
+  <div>
+    <Svga src={winIcon} option={{ loop: true }} on={onHandle} />
+    <Svga src={loseIcon} option={{ loop: true }} on={onHandle} />
+  </div>
+)
 
 ReactDOM.render(App, document.querySelector('#app'))
